c432c4b857c6b864033feb719634417871e63368
Diego Pagini <dq@qbkconsulting.com>
2021-03-12 08:06:22 -0300

    face and following eyes

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/** @format */
+
+const container = document.querySelector('.color-change');
+container.addEventListener('mousemove', (e) => {
+	const eyes = document.querySelectorAll('.eye');
+	[].forEach.call(eyes, function (eye) {
+		let mouseX = eye.getBoundingClientRect().right;
+		if (eye.classList.contains('left-eye')) {
+			mouseX = eye.getBoundingClientRect().left;
+		}
+		let mouseY = eye.getBoundingClientRect().top;
+		let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
+		let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
+		eye.style.transform = `rotate(${rotationDegrees}deg)`;
+	});
+});
+
+console.log('Hola Mundo!');
